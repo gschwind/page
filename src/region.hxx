9f204915fa89e5e91659781479e586171d3c7a53
Benoit Gschwind <gschwind@gnu-log.net>
2013-07-31 13:16:07 +0200

    cleanup/improve region_t template

--- src/region.hxx
+++ src/region.hxx
@@ -16,71 +16,68 @@ namespace page {
 
 template<typename T>
 class region_t: public std::list<box_t<T> > {
+	/** short cut for the superior class **/
+	typedef std::list<box_t<T> > _superior_t;
 	typedef box_t<T> _box_t;
-public:
-	typedef std::list<_box_t> box_list_t;
-private:
 
-	inline static void copy_without(typename box_list_t::const_iterator x,
-			typename box_list_t::const_iterator y, box_list_t const & list,
-			box_list_t & out) {
-		typename box_list_t::const_iterator i = list.begin();
-		while (i != list.end()) {
-			if (i != x && i != y) {
-				out.push_back(*i);
-			}
-			++i;
-		}
+	/** this function reduce the number of boxes if possible **/
+	static region_t & clean_up(region_t & lst) {
+		remove_empty(lst);
+		while (merge_area_macro(lst))
+			continue;
+		return lst;
 	}
 
-	class filter_box_t {
-		box_int_t box;
+	class _filter_box_t {
+		_box_t box0;
+		_box_t box1;
 	public:
-		bool operator() (box_int_t & x) {
-			return x == box;
+		bool operator() (_box_t const & x) {
+			return x == box0 || x == box1;
 		}
 
-		filter_box_t(box_int_t const & x) : box(x) { }
+		_filter_box_t(_box_t const & x, _box_t const & y) : box0(x), box1(y) { }
+
 	};
 
-	/* merge 2 rectangle, not efficiently */
-	static bool merge_area_macro(box_list_t & list) {
-		typename box_list_t::const_iterator i = list.begin();
+	/** merge 2 rectangles when it is possible **/
+	static bool merge_area_macro(region_t & list) {
+		typename region_t::const_iterator i = list.begin();
 		while (i != list.end()) {
-			typename box_list_t::const_iterator j = list.begin();
+			typename region_t::const_iterator j = list.begin();
 			while (j != list.end()) {
 				if (i != j) {
-					box_int_t bi = *i;
-					box_int_t bj = *j;
+					_box_t bi = *i;
+					_box_t bj = *j;
 
+					/** left/right **/
 					if (bi.x + bi.w == bj.x && bi.y == bj.y && bi.h == bj.h) {
-						list.remove_if(filter_box_t(bi));
-						list.remove_if(filter_box_t(bj));
-						box_int_t new_box(bi.x, bi.y, bj.w + bi.w, bi.h);
+						list.remove_if(_filter_box_t(bi, bj));
+						_box_t new_box(bi.x, bi.y, bj.w + bi.w, bi.h);
 						list.push_back(new_box);
 						return true;
 					}
 
+					/** right/left **/
 					if (bi.x == bj.x + bj.w && bi.y == bj.y && bi.h == bj.h) {
-						list.remove_if(filter_box_t(bi));
-						list.remove_if(filter_box_t(bj));
-						box_int_t new_box(bj.x, bj.y, bj.w + bi.w, bj.h);
+						list.remove_if(_filter_box_t(bi, bj));
+						_box_t new_box(bj.x, bj.y, bj.w + bi.w, bj.h);
 						list.push_back(new_box);
 						return true;
 					}
 
+					/** top/bottom **/
 					if (bi.y == bj.y + bj.h && bi.x == bj.x && bi.w == bj.w) {
-						list.remove_if(filter_box_t(bi));
-						list.remove_if(filter_box_t(bj));
-						box_int_t new_box(bj.x, bj.y, bj.w, bj.h + bi.h);
+						list.remove_if(_filter_box_t(bi, bj));
+						_box_t new_box(bj.x, bj.y, bj.w, bj.h + bi.h);
 						list.push_back(new_box);
 						return true;
 					}
 
+					/** bottom/top **/
 					if (bi.y + bi.h == bj.y && bi.x == bj.x && bi.w == bj.w) {
-						list.remove_if(filter_box_t(bi));
-						list.remove_if(filter_box_t(bj));
-						box_int_t new_box(bi.x, bi.y, bi.w, bj.h + bi.h);
+						list.remove_if(_filter_box_t(bi, bj));
+						_box_t new_box(bi.x, bi.y, bi.w, bj.h + bi.h);
 						list.push_back(new_box);
 						return true;
 					}
@@ -95,11 +92,12 @@ private:
 
 	}
 
-	static bool _is_null(box_int_t & b) {
+	static bool _is_null(_box_t const & b) {
 		return b.is_null();
 	}
 
-	static void remove_empty(box_list_t & list) {
+	/** remove empty boxes **/
+	static void remove_empty(region_t & list) {
 		list.remove_if(_is_null);
 	}
 
@@ -108,117 +106,114 @@ private:
 	}
 
 	/* box0 - box1 */
-	static region_t::box_list_t substract_box(_box_t const & box0,
+	static region_t substract_box(_box_t const & box0,
 			_box_t const & box1) {
-		//std::cout <<"a "<< box0.to_string() << std::endl;
-		//std::cout <<"b "<< box1.to_string() << std::endl;
-		box_list_t result;
+		region_t result;
 
-		box_int_t inter_sec = box0 & box1;
-		//std::cout <<"c "<< inter_sec.to_string() << std::endl;
+		_box_t intersection = box0 & box1;
 
-		if (inter_sec.w > 0 && inter_sec.h > 0) {
+		if (!intersection.is_null()) {
 			/* top box */
 			{
-				int left = inter_sec.x;
-				int right = inter_sec.x + inter_sec.w;
-				int top = box0.y;
-				int bottom = inter_sec.y;
+				T left = intersection.x;
+				T right = intersection.x + intersection.w;
+				T top = box0.y;
+				T bottom = intersection.y;
 
 				if (right - left > 0 && bottom - top > 0) {
 					result.push_back(
-							box_int_t(left, top, right - left, bottom - top));
+							_box_t(left, top, right - left, bottom - top));
 				}
 			}
 
 			/* bottom box */
 			{
-				int left = inter_sec.x;
-				int right = inter_sec.x + inter_sec.w;
-				int top = inter_sec.y + inter_sec.h;
-				int bottom = box0.y + box0.h;
+				T left = intersection.x;
+				T right = intersection.x + intersection.w;
+				T top = intersection.y + intersection.h;
+				T bottom = box0.y + box0.h;
 
 				if (right - left > 0 && bottom - top > 0) {
 					result.push_back(
-							box_int_t(left, top, right - left, bottom - top));
+							_box_t(left, top, right - left, bottom - top));
 				}
 			}
 
 			/* left box */
 			{
-				int left = box0.x;
-				int right = inter_sec.x;
-				int top = inter_sec.y;
-				int bottom = inter_sec.y + inter_sec.h;
+				T left = box0.x;
+				T right = intersection.x;
+				T top = intersection.y;
+				T bottom = intersection.y + intersection.h;
 
 				if (right - left > 0 && bottom - top > 0) {
 					result.push_back(
-							box_int_t(left, top, right - left, bottom - top));
+							_box_t(left, top, right - left, bottom - top));
 				}
 			}
 
 			/* right box */
 			{
-				int left = inter_sec.x + inter_sec.w;
-				int right = box0.x + box0.w;
-				int top = inter_sec.y;
-				int bottom = inter_sec.y + inter_sec.h;
+				T left = intersection.x + intersection.w;
+				T right = box0.x + box0.w;
+				T top = intersection.y;
+				T bottom = intersection.y + intersection.h;
 
 				if (right - left > 0 && bottom - top > 0) {
 					result.push_back(
-							box_int_t(left, top, right - left, bottom - top));
+							_box_t(left, top, right - left, bottom - top));
 				}
 			}
 
 			/* top left box */
 			{
-				int left = box0.x;
-				int right = inter_sec.x;
-				int top = box0.y;
-				int bottom = inter_sec.y;
+				T left = box0.x;
+				T right = intersection.x;
+				T top = box0.y;
+				T bottom = intersection.y;
 
 				if (right - left > 0 && bottom - top > 0) {
 					result.push_back(
-							box_int_t(left, top, right - left, bottom - top));
+							_box_t(left, top, right - left, bottom - top));
 				}
 			}
 
 			/* top right box */
 			{
-				int left = inter_sec.x + inter_sec.w;
-				int right = box0.x + box0.w;
-				int top = box0.y;
-				int bottom = inter_sec.y;
+				T left = intersection.x + intersection.w;
+				T right = box0.x + box0.w;
+				T top = box0.y;
+				T bottom = intersection.y;
 
 				if (right - left > 0 && bottom - top > 0) {
 					result.push_back(
-							box_int_t(left, top, right - left, bottom - top));
+							_box_t(left, top, right - left, bottom - top));
 				}
 			}
 
 			/* bottom left box */
 			{
-				int left = box0.x;
-				int right = inter_sec.x;
-				int top = inter_sec.y + inter_sec.h;
-				int bottom = box0.y + box0.h;
+				T left = box0.x;
+				T right = intersection.x;
+				T top = intersection.y + intersection.h;
+				T bottom = box0.y + box0.h;
 
 				if (right - left > 0 && bottom - top > 0) {
 					result.push_back(
-							box_int_t(left, top, right - left, bottom - top));
+							_box_t(left, top, right - left, bottom - top));
 				}
 			}
 
 			/* bottom right box */
 			{
-				int left = inter_sec.x + inter_sec.w;
-				int right = box0.x + box0.w;
-				int top = inter_sec.y + inter_sec.h;
-				int bottom = box0.y + box0.h;
+				T left = intersection.x + intersection.w;
+				T right = box0.x + box0.w;
+				T top = intersection.y + intersection.h;
+				T bottom = box0.y + box0.h;
 
 				if (right - left > 0 && bottom - top > 0) {
 					result.push_back(
-							box_int_t(left, top, right - left, bottom - top));
+							_box_t(left, top, right - left, bottom - top));
 				}
 			}
 
@@ -226,155 +221,160 @@ private:
 			result.push_back(box0);
 		}
 
-		while (merge_area_macro(result))
-			continue;
-
-		remove_empty(result);
-
-		return result;
+		return clean_up(result);
 
 	}
 
 public:
 
-	region_t() {
-
-	}
+	/** create an empty region **/
+	region_t() { }
 
 	region_t(region_t const & r) :
-			box_list_t(r) {
+			_superior_t(r) {
 	}
 
-	region_t(box_t<T> const & b) {
+	region_t(_box_t const & b) {
 		if (!b.is_null())
 			this->push_back(b);
 	}
 
 	region_t(T x, T y, T w, T h) {
-		box_t<T> b(x, y, w, h);
+		_box_t b(x, y, w, h);
 		if (!b.is_null())
 			this->push_back(b);
 	}
 
-	region_t & operator=(box_t<T> const & b) {
-		box_list_t::clear();
+	region_t & operator =(_box_t const & b) {
+		this->clear();
 		if (!b.is_null())
 			this->push_back(b);
 		return *this;
 	}
 
-	region_t operator-(box_t<T> const & box1) const {
-		region_t result;
-		typename box_list_t::const_iterator i = box_list_t::begin();
-		while (i != box_list_t::end()) {
-			region_t::box_list_t x = substract_box(*i, box1);
-			result.insert(result.end(), x.begin(), x.end());
-			++i;
-		}
-
-		while (merge_area_macro(result))
-			continue;
-
-		remove_empty(result);
-
-		return result;
+	region_t & operator =(region_t const & r) {
+		/** call the superior class assign operator **/
+		this->_superior_t::operator =(r);
+		return *this;
 	}
 
-	region_t & operator-=(box_t<T> const & b) {
+	region_t operator -(_box_t const & box1) const {
+		region_t result;
+		for(typename region_t::const_iterator i = this->begin(); i != this->end(); ++i) {
+			region_t x = substract_box(*i, box1);
+			result.insert(result.end(), x.begin(), x.end());
+		}
+		return clean_up(result);
+	}
+
+	region_t & operator -=(_box_t const & b) {
 		*this = *this - b;
 		return *this;
 	}
 
-	region_t operator-(region_t const & b) const {
+	region_t operator -(region_t const & b) const {
 		region_t result = *this;
-		typename box_list_t::const_iterator i = b.begin();
-		while (i != b.end()) {
+		for (typename region_t::const_iterator i = b.begin(); i != b.end(); ++i) {
 			result -= *i;
-			++i;
 		}
 
 		return result;
 	}
 
-	region_t & operator+=(box_t<T> const & b) {
+	region_t & operator +=(_box_t const & b) {
 		if (!b.is_null()) {
-			//std::cout << "XX this =" << this->to_string() << std::endl;
 			*this -= b;
-			//std::cout << "X@ this =" << this->to_string() << std::endl;
 			this->push_back(b);
-			//std::cout << "X# this =" << this->to_string() << std::endl;
-			while (merge_area_macro(*this))
-				continue;
-
-			remove_empty(*this);
+			clean_up(*this);
 		}
-		//std::cout << "X%% this =" << this->to_string() << std::endl;
 		return *this;
 	}
 
-	region_t operator+(box_t<T> const & b) const {
-		region_t result = *this;
-		result += b;
-		return result;
-	}
+	region_t & operator +=(region_t const & r) {
+		/** sum same regions give same region **/
+		if(this == &r)
+			return *this;
 
-	region_t operator+(region_t const & r) const {
-		region_t result = *this;
-		typename box_list_t::const_iterator i = r.begin();
-		while (i != r.end()) {
-			result += *i;
-			++i;
+		for(typename region_t::const_iterator i = r.begin(); i != r.end(); ++i) {
+			*this += *i;
 		}
-		//std::cout << "X$ this =" << result.to_string() << std::endl;
-		return result;
+
+		return *this;
 	}
 
-	region_t<T> operator&(region_t<T> const & r) const {
-		region_t<T> result;
-		typename region_t<T>::box_list_t::const_iterator i, j;
-		i = box_list_t::begin();
-		while (i != box_list_t::end()) {
-			j = r.begin();
-			while (j != r.end()) {
-				box_int_t v = (*i & *j);
-				if (!v.is_null()) {
-					result = result + v;
-				}
-				++j;
+	region_t operator +(_box_t const & b) const {
+		region_t result = *this;
+		return result += b;
+	}
+
+	region_t operator +(region_t const & r) const {
+		region_t result = *this;
+		return result += r;
+	}
+
+	region_t operator &(_box_t const & b) const {
+		region_t result;
+		for(typename region_t::const_iterator i = this->begin(); i != this->end(); ++i) {
+			_box_t x = b & *i;
+			/**
+			 * since this is a region, no over lap is possible, just add the
+			 * intersection if not null. (do not use operator+= for optimal
+			 * result)
+			 **/
+			if(!x.is_null()) {
+				result.push_back(x);
 			}
-			++i;
 		}
-		return result;
+		return clean_up(result);
+	}
+
+	region_t operator &(region_t const & r) const {
+		region_t result;
+
+		for(typename region_t::const_iterator i = this->begin(); i != this->end(); ++i) {
+			region_t clipped = r & *i;
+			result.insert(result.begin(), clipped.begin(), clipped.end());
+		}
+
+		return clean_up(result);
+	}
+
+	region_t & operator &=(_box_t const & b) {
+		*this = *this & b;
+		return *this;
+	}
+
+	region_t & operator &=(region_t const & r) {
+		if(this == &r)
+			return*this;
+		*this = *this & r;
+		return *this;
 	}
 
 
+	/** make string **/
 	std::string to_string() const {
 		std::ostringstream os;
-		//os << area.size();
-		typename box_list_t::const_iterator i = box_list_t::begin();
-		while (i != box_list_t::end()) {
-			if (i != box_list_t::begin())
+		for(typename region_t::const_iterator i = this->begin(); i != this->end(); ++i) {
+			if (i != this->begin())
 				os << ",";
 			os << (*i).to_string();
-			++i;
 		}
-
 		return os.str();
-
 	}
 
 	void translate(T x, T y) {
-		typename box_list_t::iterator i = box_list_t::begin();
-		while (i != box_list_t::end()) {
+		for(typename region_t::iterator i = this->begin(); i != this->end(); ++i) {
 			(*i).x += x;
 			(*i).y += y;
-			++i;
 		}
 	}
 
 };
 
-
+/** instanciate template for checking purpose **/
+template class region_t<int>;
+template class region_t<double>;
 
 }
 
