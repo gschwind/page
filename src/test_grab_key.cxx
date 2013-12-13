e2fcdccb7040e29b66a2d9fc01272f7199f6e0df
Benoit Gschwind <gschwind@gnu-log.net>
2013-12-13 22:40:05 +0100

    add missing testing code

---
+++ src/test_grab_key.cxx
@@ -0,0 +1,56 @@
+/*
+ * test_grab_key.cxx
+ *
+ *  Created on: 11 déc. 2013
+ *      Author: benoit.gschwind
+ */
+
+#include <X11/Xlib.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
+#include <cstdio>
+
+int main() {
+
+	Display * dpy;
+	dpy = XOpenDisplay(0);
+	Window root = XDefaultRootWindow(dpy);
+	XGrabKey(dpy, XKeysymToKeycode(dpy, XK_y), Mod4Mask, root, 0, GrabModeAsync, GrabModeSync);
+
+	while(true) {
+
+		XEvent ev;
+		XNextEvent(dpy, &ev);
+		if(ev.type == KeyPress or ev.type == KeyRelease) {
+
+			if (ev.xkey.keycode == 29
+					and ev.type == KeyPress and (ev.xkey.state == Mod4Mask)) {
+				XGrabKeyboard(dpy, root, 0, GrabModeAsync, GrabModeAsync,
+						ev.xkey.time);
+			}
+
+			if (ev.xkey.keycode == 133 and ev.type == KeyRelease) {
+				XUngrabKeyboard(dpy, ev.xkey.time);
+			}
+
+			XAllowEvents(dpy, AsyncKeyboard, ev.xkey.time);
+
+			if (ev.type == KeyPress) {
+				printf("KeyPress key = %d, mod4 = %s, mod1 = %s\n", ev.xkey.keycode,
+						ev.xkey.state & Mod4Mask ? "true" : "false",
+						ev.xkey.state & Mod1Mask ? "true" : "false");
+			} else {
+				printf("KeyRelease key = %d, mod4 = %s, mod1 = %s\n",
+						ev.xkey.keycode, ev.xkey.state & Mod4Mask ? "true" : "false",
+						ev.xkey.state & Mod1Mask ? "true" : "false");
+			}
+
+
+		}
+
+	}
+
+
+
+
+}
