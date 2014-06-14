5d86f6bb69211e8d8b699c9f3f33dfa41f5a7138
Benoit Gschwind <gschwind@gnu-log.net>
2014-06-14 19:02:04 +0200

    cleanup notebook.cxx and notebook.hxx

--- src/notebook.cxx
+++ src/notebook.cxx
@@ -320,5 +320,184 @@ void notebook_t::set_theme(theme_t const * theme) {
 	_theme = theme;
 }
 
+list<managed_window_base_t const *> notebook_t::clients() const {
+	return list<managed_window_base_t const *>(_clients.begin(), _clients.end());
+}
+
+managed_window_base_t const * notebook_t::selected() const {
+	if(_selected.empty()) {
+		return nullptr;
+	} else {
+		return _selected.front();
+	}
+}
+
+bool notebook_t::is_default() const {
+	return _is_default;
+}
+
+void notebook_t::set_default(bool x) {
+	_is_default = x;
+}
+
+list<tree_t *> notebook_t::childs() const {
+	list<tree_t *> ret(_children.begin(), _children.end());
+	return ret;
+}
+
+void notebook_t::raise_child(tree_t * t) {
+
+	if (has_key(_children, t)) {
+		_children.remove(t);
+		_children.push_back(t);
+	}
+
+	if(_parent != nullptr) {
+		_parent->raise_child(this);
+	}
+
+}
+
+string notebook_t::get_node_name() const {
+	return _get_node_name<'N'>();
+}
+
+void notebook_t::render_legacy(cairo_t * cr, rectangle const & area) const {
+	_theme->render_notebook(cr, this, area);
+}
+
+void notebook_t::render(cairo_t * cr, time_t time) {
+
+	//_theme->render_notebook(cr, this, _allocation);
+
+	if (not _selected.empty()) {
+
+		page::time_t d(0, animation_duration);
+		if (time < (swap_start + d)) {
+			double ratio = static_cast<double>(time - swap_start)/animation_duration;
+
+			double y = floor(ratio * _allocation.h);
+			if(y < 0.0)
+				y = 0.0;
+			if(y > _allocation.h)
+				y = _allocation.h;
+
+			managed_window_t * mw = _selected.front();
+			composite_surface_handler_t psurf = mw->surf();
+
+			if (prev_surf != nullptr) {
+				cairo_surface_t * s = prev_surf->get_surf();
+				rectangle location = prev_loc;
+				display_t::create_context(__FILE__, __LINE__);
+				cairo_save(cr);
+				cairo_rectangle(cr, location.x, location.y, location.w,
+						location.h);
+				cairo_clip(cr);
+				cairo_set_source_surface(cr, s, location.x, location.y);
+				cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+				cairo_pattern_t * p0 = cairo_pattern_create_linear(0.0,
+						y - 150.0, 0.0, y);
+				cairo_pattern_add_color_stop_rgba(p0, 0.0, 1.0, 1.0, 1.0,
+						0.0);
+				cairo_pattern_add_color_stop_rgba(p0, 1.0, 1.0, 1.0, 1.0,
+						1.0);
+				cairo_mask(cr, p0);
+				cairo_pattern_destroy(p0);
+				cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
+				cairo_pattern_t * p1 = cairo_pattern_create_linear(0.0,
+						y - 150.0, 0.0, y);
+				cairo_pattern_add_color_stop_rgba(p1, 0.0, 1.0, 1.0, 1.0,
+						0.0);
+				cairo_pattern_add_color_stop_rgba(p1, 1.0, 1.0, 1.0, 1.0,
+						1.0);
+				cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, ratio*0.1);
+				cairo_mask(cr, p1);
+				cairo_pattern_destroy(p1);
+
+				display_t::destroy_context(__FILE__, __LINE__);
+				cairo_restore(cr);
+			}
+
+			if (psurf != nullptr) {
+				cairo_surface_t * s = psurf->get_surf();
+				rectangle old = mw->base_position();
+				rectangle location = mw->base_position();
+				display_t::create_context(__FILE__, __LINE__);
+				cairo_save(cr);
+				cairo_rectangle(cr, location.x, location.y, location.w,
+						location.h);
+				cairo_clip(cr);
+				cairo_set_source_surface(cr, s, location.x, location.y);
+				cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+				cairo_pattern_t * p = cairo_pattern_create_linear(0.0,
+						y, 0.0, y + 150.0);
+				cairo_pattern_add_color_stop_rgba(p, 1.0, 1.0, 1.0, 1.0,
+						0.0);
+				cairo_pattern_add_color_stop_rgba(p, 0.0, 1.0, 1.0, 1.0,
+						1.0);
+				cairo_mask(cr, p);
+				cairo_pattern_destroy(p);
+				display_t::destroy_context(__FILE__, __LINE__);
+				cairo_restore(cr);
+
+			}
+
+
+
+			for (auto i : mw->childs()) {
+				i->render(cr, time);
+			}
+
+
+		} else {
+
+			prev_surf.reset();
+
+			managed_window_t * mw = _selected.front();
+			composite_surface_handler_t psurf = mw->surf();
+			if (psurf != nullptr) {
+				cairo_surface_t * s = psurf->get_surf();
+				rectangle location = mw->get_base_position();
+				display_t::create_context(__FILE__, __LINE__);
+
+				cairo_save(cr);
+				cairo_set_source_surface(cr, s, location.x, location.y);
+				cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+				cairo_rectangle(cr, location.x, location.y, location.w,
+						location.h);
+				cairo_fill(cr);
+				display_t::destroy_context(__FILE__, __LINE__);
+				cairo_restore(cr);
+
+			}
+
+			for (auto i : mw->childs()) {
+				i->render(cr, time);
+			}
+
+		}
+
+
+
+	}
+
+}
+
+bool notebook_t::need_render(time_t time) {
+
+	page::time_t d(0, animation_duration);
+	if (time < (swap_start + d)) {
+		return true;
+	}
+
+	for(auto i: childs()) {
+		if(i->need_render(time)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
 
 }
