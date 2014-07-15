eaeb22dc11e9a0265175b0a9d571988827439de8
Benoit Gschwind <gschwind@gnu-log.net>
2014-07-15 08:55:14 +0200

    add a missing file

---
+++ src/key_desc.hxx
@@ -0,0 +1,23 @@
+/*
+ * key_desc.hxx
+ *
+ *  Created on: 12 juil. 2014
+ *      Author: gschwind
+ */
+
+#ifndef KEY_DESC_HXX_
+#define KEY_DESC_HXX_
+
+
+namespace page {
+
+struct key_desc_t {
+	KeySym ks;
+	unsigned int mod;
+};
+
+
+}
+
+
+#endif /* KEY_DESC_HXX_ */
