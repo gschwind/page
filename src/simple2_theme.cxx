c221dfe216e5369e8569a08002cf00ccd9d13109
Benoit Gschwind <gschwind@gnu-log.net>
2014-08-02 14:14:42 +0200

    minor update in simple2_theme margin

--- src/simple2_theme.cxx
+++ src/simple2_theme.cxx
@@ -84,7 +84,7 @@ rectangle simple2_theme_t::compute_floating_bind_position(
 
 simple2_theme_t::simple2_theme_t(display_t * cnx, config_handler_t & conf) {
 
-	notebook_margin.top = 24;
+	notebook_margin.top = 22;
 	notebook_margin.bottom = 4;
 	notebook_margin.left = 4;
 	notebook_margin.right = 4;
