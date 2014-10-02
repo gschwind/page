e3d64d5591546a9642586cf440b534e91a5441ed
Benoit Gschwind <gschwind@gnu-log.net>
2014-10-02 18:46:49 +1000

    add a page exception handler using snprintf

---
+++ src/exception.hxx
@@ -0,0 +1,84 @@
+/*
+ * exception.hxx
+ *
+ *  Created on: 1 oct. 2014
+ *      Author: gschwind
+ */
+
+#ifndef EXCEPTION_HXX_
+#define EXCEPTION_HXX_
+
+#include <cstdarg>
+#include <cstdio>
+#include <exception>
+
+namespace page {
+
+using namespace std;
+
+
+
+
+
+class exception_t : public std::exception {
+	char * str;
+public:
+	exception_t(char const * fmt, ...) : str(nullptr) {
+		va_list l;
+		va_start(l, fmt);
+		int n = vsnprintf(nullptr, 0, fmt, l);
+		va_end(l);
+		str = new char[n];
+		va_start(l, fmt);
+		vsnprintf(str, n, fmt, l);
+		va_end(l);
+	}
+
+	~exception_t() noexcept { delete[] str; }
+
+	char const * what() const noexcept {
+		return str;
+	}
+
+};
+
+
+
+///** not define **/
+//template <typename T>
+//class exception_t<T>;
+//
+//template <>
+//class exception_t<char const *> : public exception {
+//	char const * data;
+//public:
+//	exception_t(char const * data) : data(data) { }
+//
+//	char const * what() {
+//		static char error_buffer[4096];
+//		snprintf(error_buffer, 4096, fmt, key);
+//		return error_buffer;
+//	}
+//
+//};
+
+//
+//class configuration_error_t : public exception {
+//	char const * fmt;
+//	char const * key;
+//public:
+//	configuration_error_t(char const * fmt, char const * key) : fmt(fmt), key(key) { }
+//
+//	char const * what() {
+//		static char error_buffer[4096];
+//		snprintf(error_buffer, 4096, fmt, key);
+//		return error_buffer;
+//	}
+//
+//};
+
+}
+
+
+
+#endif /* EXCEPTION_HXX_ */
