b2e7ba6650c62d2d3a0bf80819505366c2755aac
Benoit Gschwind <gschwind@gnu-log.net>
2015-08-01 11:29:44 +0200

    fix default pop button update

--- src/notebook.cxx
+++ src/notebook.cxx
@@ -335,6 +335,8 @@ bool notebook_t::is_default() const {
 
 void notebook_t::set_default(bool x) {
 	_is_default = x;
+	theme_notebook.is_default = _is_default;
+	queue_redraw();
 }
 
 void notebook_t::activate(tree_t * t) {
@@ -540,14 +542,6 @@ void notebook_t::_update_notebook_areas() {
 
 	}
 }
-
-
-//void notebook_t::get_all_children(std::vector<tree_t *> & out) const {
-//	for(auto x: _children) {
-//		out.push_back(x);
-//		x->get_all_children(out);
-//	}
-//}
 
 void notebook_t::_update_theme_notebook(theme_notebook_t & theme_notebook) const {
 	theme_notebook.clients_tab.clear();
