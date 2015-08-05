b4d780e2ad3e7335a9d1c0b4d565c7f53aee9587
Benoit Gschwind <gschwind@gnu-log.net>
2015-08-05 23:36:00 +0200

    more tree

--- src/tree.cxx
+++ src/tree.cxx
@@ -7,7 +7,6 @@
  *
  */
 
-
 #include "tree.hxx"
 
 #include <cassert>
@@ -16,5 +15,254 @@
 
 namespace page {
 
+tree_t::tree_t() :
+		_parent { }, _is_visible { false } {
+}
+
+tree_t::~tree_t() {
+}
+
+/**
+ * Return parent within tree
+ **/
+auto tree_t::parent() const -> weak_ptr<tree_t> {
+	return _parent;
+}
+
+/**
+ * Change parent of this node to parent.
+ **/
+void tree_t::set_parent(shared_ptr<tree_t> parent) {
+	_parent = parent;
+}
+
+void tree_t::clear_parent() {
+	_parent.reset();
+}
+
+/**
+ * Hide this node recursively
+ **/
+void tree_t::hide() {
+	_is_visible = false;
+}
+
+/**
+ * Show this node recursively
+ **/
+void tree_t::show() {
+	_is_visible = true;
+}
+
+/**
+ * Return the name of this tree node
+ **/
+auto tree_t::get_node_name() const -> string {
+	return string { "ANONYMOUS NODE" };
+}
+
+/**
+ * Remove i from _direct_ child of this node *not recusively*
+ **/
+void tree_t::remove(shared_ptr<tree_t> t) {
+	/* because by default we have no child, by default remove do nothing */
+}
+
+/**
+ * Return direct children of this node (net recursive)
+ **/
+auto tree_t::children(vector<shared_ptr<tree_t>> & out) const -> void {
+	/* by default we have no child */
+}
+
+/**
+ * return the list of renderable object to draw this tree ordered and recursively
+ **/
+auto tree_t::update_layout(time64_t const time) -> void {
+	/* by default do not update anything */
+}
+
+/**
+ * draw the area of a renderable to the destination surface
+ * @param cr the destination surface context
+ * @param area the area to redraw
+ **/
+void tree_t::render(cairo_t * cr, region const & area) {
+	/* by default tree_t do not render any thing */
+}
+
+/**
+ * Derived class must return opaque region for this object,
+ * If unknown it's safe to leave this empty.
+ **/
+region tree_t::get_opaque_region() {
+	/* by default tree_t is invisible */
+	return region { };
+}
+
+/**
+ * Derived class must return visible region,
+ * If unknown the whole screen can be returned, but draw will be called each time.
+ **/
+region tree_t::get_visible_region() {
+	/* by default tree_t is invisible */
+	return region { };
+}
+
+/**
+ * return currently damaged area (absolute)
+ **/
+region tree_t::get_damaged() {
+	/* by default tree_t has no damage */
+	return region { };
+}
+
+/**
+ * make the component active.
+ **/
+void tree_t::activate(shared_ptr<tree_t> t) {
+}
+
+bool tree_t::button_press(xcb_button_press_event_t const * ev) {
+	return false;
+}
+bool tree_t::button_release(xcb_button_release_event_t const * ev) {
+	return false;
+}
+bool tree_t::button_motion(xcb_motion_notify_event_t const * ev) {
+	return false;
+}
+bool tree_t::leave(xcb_leave_notify_event_t const * ev) {
+	return false;
+}
+bool tree_t::enter(xcb_enter_notify_event_t const * ev) {
+	return false;
+}
+void tree_t::expose(xcb_expose_event_t const * ev) {
+}
+
+void tree_t::trigger_redraw() {
+}
+
+/**
+ * return the root top level xid or XCB_WINDOW_NONE if not applicable.
+ **/
+xcb_window_t tree_t::get_xid() const {
+	return XCB_WINDOW_NONE;
+}
+
+xcb_window_t tree_t::get_parent_xid() const {
+	if (not _parent.expired())
+		return _parent.lock()->get_parent_xid();
+	else
+		return XCB_WINDOW_NONE;
+}
+
+rect tree_t::get_window_position() const {
+	if (not _parent.expired())
+		return _parent.lock()->get_window_position();
+	else
+		return rect { };
+}
+
+void tree_t::queue_redraw() {
+	if (not _parent.expired())
+		_parent.lock()->queue_redraw();
+}
+
+/**
+ * Print the tree recursively using node names.
+ **/
+void tree_t::print_tree(int level) const {
+	char space[] = "                               ";
+	space[level] = 0;
+	cout << space << get_node_name() << endl;
+	for (auto i : children()) {
+		i->print_tree(level + 1);
+	}
+}
+
+/**
+ * Short cut to children(std::vector<tree_t *> & out)
+ **/
+vector<shared_ptr<tree_t>> tree_t::children() const {
+	vector<shared_ptr<tree_t>> ret;
+	children(ret);
+	return ret;
+}
+
+/**
+ * get all children recursively
+ **/
+void tree_t::get_all_children_deep_first(
+		vector<shared_ptr<tree_t>> & out) const {
+	for (auto x : children()) {
+		x->get_all_children_deep_first(out);
+		out.push_back(x);
+	}
+}
+
+void tree_t::get_all_children_root_first(
+		vector<shared_ptr<tree_t>> & out) const {
+	for (auto x : children()) {
+		out.push_back(x);
+		x->get_all_children_root_first(out);
+	}
+}
+
+vector<shared_ptr<tree_t>> tree_t::get_all_children() const {
+	vector<shared_ptr<tree_t>> ret;
+	get_all_children_root_first(ret);
+	return ret;
+}
+
+vector<shared_ptr<tree_t>> tree_t::get_all_children_deep_first() const {
+	vector<shared_ptr<tree_t>> ret;
+	get_all_children_deep_first(ret);
+	return ret;
+}
+
+vector<shared_ptr<tree_t>> tree_t::get_all_children_root_first() const {
+	vector<shared_ptr<tree_t>> ret;
+	get_all_children_root_first(ret);
+	return ret;
+}
+
+void tree_t::broadcast_trigger_redraw() {
+	_broadcast_deep_first(&tree_t::trigger_redraw);
+}
+
+bool tree_t::broadcast_button_press(xcb_button_press_event_t const * ev) {
+	return _broadcast_deep_first(&tree_t::button_press, ev);
+}
+
+bool tree_t::broadcast_button_release(xcb_button_release_event_t const * ev) {
+	return _broadcast_deep_first(&tree_t::button_release, ev);
+}
+
+bool tree_t::broadcast_button_motion(xcb_motion_notify_event_t const * ev) {
+	return _broadcast_deep_first(&tree_t::button_motion, ev);
+}
+
+bool tree_t::broadcast_leave(xcb_leave_notify_event_t const * ev) {
+	return _broadcast_deep_first(&tree_t::leave, ev);
+}
+
+bool tree_t::broadcast_enter(xcb_enter_notify_event_t const * ev) {
+	return _broadcast_deep_first(&tree_t::enter, ev);
+}
+
+void tree_t::broadcast_expose(xcb_expose_event_t const * ev) {
+	_broadcast_deep_first(&tree_t::expose, ev);
+}
+
+void tree_t::broadcast_update_layout(time64_t const time) {
+	_broadcast_root_first(&tree_t::update_layout, time);
+}
+
+rect tree_t::to_root_position(rect const & r) const {
+	return rect { r.x + get_window_position().x, r.y + get_window_position().y,
+			r.w, r.h };
+}
 
 }
--- src/tree.hxx
+++ src/tree.hxx
@@ -13,6 +13,7 @@
 #include <memory>
 #include <iostream>
 
+#include "utils.hxx"
 #include "renderable.hxx"
 #include "time.hxx"
 
@@ -26,16 +27,13 @@ using namespace std;
  * It define the stack order of each component drawn within page.
  **/
 class tree_t : public enable_shared_from_this<tree_t> {
+
 protected:
-	weak_ptr<tree_t> _parent;
-
-	bool _is_visible;
-
 	template<typename ... T>
 	bool _broadcast_root_first(bool (tree_t::* f)(T ... args), T ... args) {
 		if((this->*f)(args...))
 			return true;
-		for(auto x: tree_t::get_all_children_root_first()) {
+		for(auto x: weak(get_all_children_root_first())) {
 			if((x->*f)(args...))
 				return true;
 		}
@@ -45,7 +43,7 @@ protected:
 	template<typename ... T>
 	bool _broadcast_root_first(void (tree_t::* f)(T ... args), T ... args) {
 		(this->*f)(args...);
-		for(auto x: tree_t::get_all_children_root_first()) {
+		for(auto x: weak(get_all_children_root_first())) {
 			if(not x.expired())
 				(x.lock().get()->*f)(args...);
 		}
@@ -53,7 +51,7 @@ protected:
 
 	template<typename ... T>
 	bool _broadcast_deep_first(bool (tree_t::* f)(T ... args), T ... args) {
-		for(auto x: tree_t::get_all_children_deep_first()) {
+		for(auto x: weak(get_all_children_deep_first())) {
 			if(not x.expired()) {
 				if((x.lock().get()->*f)(args...))
 					return true;
@@ -66,158 +64,13 @@ protected:
 
 	template<typename ... T>
 	void _broadcast_deep_first(void (tree_t::* f)(T ... args), T ... args) {
-		for(auto x: tree_t::get_all_children_deep_first()) {
+		for(auto x: weak(get_all_children_deep_first())) {
 			if(not x.expired())
 				(x.lock().get()->*f)(args...);
 		}
 		(this->*f)(args...);
 	}
 
-	bool _trigger_redraw_handler() { trigger_redraw(); return false; }
-
-public:
-	tree_t() : _parent{}, _is_visible{false} { }
-
-	virtual ~tree_t() { }
-
-	/**
-	 * Return parent within tree
-	 **/
-	auto parent() const -> weak_ptr<tree_t> {
-		return _parent;
-	}
-
-	/**
-	 * Change parent of this node to parent.
-	 **/
-	void set_parent(weak_ptr<tree_t> parent) {
-		_parent = parent;
-	}
-
-	void clear_parent() {
-		_parent.reset();
-	}
-
-	/**
-	 * Hide this node recursively
-	 **/
-	virtual auto hide() -> void {
-		_is_visible = false;
-	}
-
-	/**
-	 * Show this node recursively
-	 **/
-	virtual auto show() -> void {
-		_is_visible = true;
-	}
-
-	/**
-	 * Return the name of this tree node
-	 **/
-	virtual auto get_node_name() const -> string {
-		return string{"ANONYMOUS NODE"};
-	}
-
-	/**
-	 * Remove i from _direct_ child of this node *not recusively*
-	 **/
-	virtual auto remove(shared_ptr<tree_t> const & t) -> void {
-		/* because by default we have no child, by default remove do nothing */
-	}
-
-
-	/**
-	 * Return direct children of this node (net recursive)
-	 **/
-	virtual auto children(vector<weak_ptr<tree_t>> & out) const -> void {
-		/* by default we have no child */
-	}
-
-	/**
-	 * return the list of renderable object to draw this tree ordered and recursively
-	 **/
-	virtual auto update_layout(time64_t const time) -> void {
-		/* by default do not update anything */
-	}
-
-	/**
-	 * draw the area of a renderable to the destination surface
-	 * @param cr the destination surface context
-	 * @param area the area to redraw
-	 **/
-	virtual void render(cairo_t * cr, region const & area) {
-		/* by default tree_t do not render any thing */
-	}
-
-	/**
-	 * Derived class must return opaque region for this object,
-	 * If unknown it's safe to leave this empty.
-	 **/
-	virtual region get_opaque_region() {
-		/* by default tree_t is invisible */
-		return region{};
-	}
-
-	/**
-	 * Derived class must return visible region,
-	 * If unknown the whole screen can be returned, but draw will be called each time.
-	 **/
-	virtual region get_visible_region() {
-		/* by default tree_t is invisible */
-		return region{};
-	}
-
-	/**
-	 * return currently damaged area (absolute)
-	 **/
-	virtual region get_damaged() {
-		/* by default tree_t has no damage */
-		return region{};
-	}
-
-	/**
-	 * make the component active.
-	 **/
-	virtual void activate(weak_ptr<tree_t> t) { }
-
-	virtual bool button_press(xcb_button_press_event_t const * ev) { return false; }
-	virtual bool button_release(xcb_button_release_event_t const * ev) { return false; }
-	virtual bool button_motion(xcb_motion_notify_event_t const * ev) { return false; }
-	virtual bool leave(xcb_leave_notify_event_t const * ev) { return false; }
-	virtual bool enter(xcb_enter_notify_event_t const * ev) { return false; }
-	virtual void expose(xcb_expose_event_t const * ev) { }
-
-	virtual void trigger_redraw() { }
-
-	/**
-	 * return the root top level xid or XCB_WINDOW_NONE if not applicable.
-	 **/
-	virtual xcb_window_t get_xid() const { return XCB_WINDOW_NONE; }
-
-	virtual xcb_window_t get_window() const {
-		if(not _parent.expired())
-			return _parent.lock()->get_window();
-		else
-			return XCB_NONE;
-	}
-
-	virtual rect get_window_position() const {
-		if(not _parent.expired())
-			return _parent.lock()->get_window_position();
-		else
-			return rect{};
-	}
-
-	virtual void queue_redraw() {
-		if (not _parent.expired())
-			_parent.lock()->queue_redraw();
-	}
-
-
-	/**
-	 * Useful template to generate node name.
-	 **/
 	template<char const c>
 	string _get_node_name() const {
 		char buffer[64];
@@ -226,105 +79,74 @@ public:
 		return string(buffer);
 	}
 
-	/**
-	 * Print the tree recursively using node names.
-	 **/
-	void print_tree(int level = 0) const {
-		char space[] = "                               ";
-		space[level] = 0;
-		cout << space << get_node_name() << endl;
-		for(auto i: children()) {
-			if(not i.expired())
-				i.lock()->print_tree(level+1);
-		}
-	}
+
+	weak_ptr<tree_t> _parent;
+	bool _is_visible;
+
+private:
+	tree_t(tree_t const &);
+	tree_t & operator=(tree_t const &);
+
+public:
+	tree_t();
+
+	auto parent() const -> weak_ptr<tree_t>;
+	void set_parent(shared_ptr<tree_t> parent);
+	void clear_parent();
+
+	void print_tree(int level = 0) const;
+
+	vector<shared_ptr<tree_t>> children() const;
+	vector<shared_ptr<tree_t>> get_all_children() const;
+	vector<shared_ptr<tree_t>> get_all_children_deep_first() const;
+	vector<shared_ptr<tree_t>> get_all_children_root_first() const;
+
+	void get_all_children_deep_first(vector<shared_ptr<tree_t>> & out) const;
+	void get_all_children_root_first(vector<shared_ptr<tree_t>> & out) const;
+
+	void broadcast_trigger_redraw();
+
+	bool broadcast_button_press(xcb_button_press_event_t const * ev);
+	bool broadcast_button_release(xcb_button_release_event_t const * ev);
+	bool broadcast_button_motion(xcb_motion_notify_event_t const * ev);
+	bool broadcast_leave(xcb_leave_notify_event_t const * ev);
+	bool broadcast_enter(xcb_enter_notify_event_t const * ev);
+	void broadcast_expose(xcb_expose_event_t const * ev);
+	void broadcast_update_layout(time64_t const time);
+
+	rect to_root_position(rect const & r) const;
 
 	/**
-	 * Short cut to children(std::vector<tree_t *> & out)
+	 * tree_t virtual API
 	 **/
-	vector<weak_ptr<tree_t>> children() const {
-		vector<weak_ptr<tree_t>> ret;
-		children(ret);
-		return ret;
-	}
 
-	/**
-	 * get all children recursively
-	 **/
-	void get_all_children_deep_first(vector<weak_ptr<tree_t>> & out) const {
-		for (auto x : children()) {
-			if (not x.expired()) {
-				x.lock()->get_all_children_deep_first(out);
-				out.push_back(x);
-			}
-		}
-	}
+	virtual ~tree_t();
+	virtual void hide();
+	virtual void show();
+	virtual auto get_node_name() const -> string;
+	virtual void remove(shared_ptr<tree_t> t);
 
-	void get_all_children_root_first(vector<weak_ptr<tree_t>> & out) const {
-		for (auto x : children()) {
-			if (not x.expired()) {
-				out.push_back(x);
-				x.lock()->get_all_children_root_first(out);
-			}
-		}
-	}
+	virtual void children(vector<shared_ptr<tree_t>> & out) const;
+	virtual void update_layout(time64_t const time);
+	virtual void render(cairo_t * cr, region const & area);
 
-	/**
-	 * Short cut to get_all_children(std::vector<tree_t *> & out)
-	 **/
-	vector<weak_ptr<tree_t>> get_all_children() const {
-		vector<weak_ptr<tree_t>> ret;
-		get_all_children_root_first(ret);
-		return ret;
-	}
+	virtual auto get_opaque_region() -> region;
+	virtual auto get_visible_region() -> region;
+	virtual auto get_damaged() -> region;
 
-	vector<weak_ptr<tree_t>> get_all_children_deep_first() const {
-		vector<weak_ptr<tree_t>> ret;
-		get_all_children_deep_first(ret);
-		return ret;
-	}
+	virtual void activate(shared_ptr<tree_t> t);
+	virtual bool button_press(xcb_button_press_event_t const * ev);
+	virtual bool button_release(xcb_button_release_event_t const * ev);
+	virtual bool button_motion(xcb_motion_notify_event_t const * ev);
+	virtual bool leave(xcb_leave_notify_event_t const * ev);
+	virtual bool enter(xcb_enter_notify_event_t const * ev);
+	virtual void expose(xcb_expose_event_t const * ev);
+	virtual void trigger_redraw();
 
-	vector<weak_ptr<tree_t>> get_all_children_root_first() const {
-		vector<weak_ptr<tree_t>> ret;
-		get_all_children_root_first(ret);
-		return ret;
-	}
-
-	void broadcast_trigger_redraw() {
-		_broadcast_deep_first(&tree_t::_trigger_redraw_handler);
-	}
-
-	bool broadcast_button_press(xcb_button_press_event_t const * ev) {
-		return _broadcast_deep_first(&tree_t::button_press, ev);
-	}
-
-	bool broadcast_button_release(xcb_button_release_event_t const * ev) {
-		return _broadcast_deep_first(&tree_t::button_release, ev);
-	}
-
-	bool broadcast_button_motion(xcb_motion_notify_event_t const * ev) {
-		return _broadcast_deep_first(&tree_t::button_motion, ev);
-	}
-
-	bool broadcast_leave(xcb_leave_notify_event_t const * ev) {
-		return _broadcast_deep_first(&tree_t::leave, ev);
-	}
-
-	bool broadcast_enter(xcb_enter_notify_event_t const * ev) {
-		return _broadcast_deep_first(&tree_t::enter, ev);
-	}
-
-	void broadcast_expose(xcb_expose_event_t const * ev) {
-		_broadcast_deep_first(&tree_t::expose, ev);
-	}
-
-	void broadcast_update_layout(time64_t const time) {
-		_broadcast_root_first(&tree_t::update_layout, time);
-	}
-
-	rect to_root_position(rect const & r) const {
-		return rect{r.x+get_window_position().x, r.y+get_window_position().y, r.w, r.h};
-	}
+	virtual auto get_xid() const -> xcb_window_t;
+	virtual auto get_parent_xid() const -> xcb_window_t;
+	virtual rect get_window_position() const;
+	virtual void queue_redraw();
 
 };
 
