d87e8c148267399eba486da7bfb2c289749d881e
Benoit Gschwind <gschwind@gnu-log.net>
2016-06-24 22:11:03 +0200

    Update src/exception.hxx copyright header

--- src/exception.hxx
+++ src/exception.hxx
@@ -1,8 +1,21 @@
 /*
- * exception.hxx
+ * Copyright (2014) Benoit Gschwind
  *
- *  Created on: 1 oct. 2014
- *      Author: gschwind
+ * This file is part of page-compositor.
+ *
+ * page-compositor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * page-compositor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with page-compositor.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef EXCEPTION_HXX_
