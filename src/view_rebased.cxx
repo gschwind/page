20219f6149008b485a06aadbaa7576b53c50f3df
Benoit Gschwind <gschwind@gnu-log.net>
2017-06-21 09:46:13 +0200

    change view_rebased_t::_ungrab_button_unsafe to grab all buttons

--- src/view_rebased.cxx
+++ src/view_rebased.cxx
@@ -172,15 +172,27 @@ void view_rebased_t::_ungrab_button_unsafe()
 	/** First ungrab all **/
 	_ungrab_all_button_unsafe();
 
-	/** grab alt-button1 move **/
-	_base->_window->grab_button(true, DEFAULT_BUTTON_EVENT_MASK,
-			XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_ASYNC, XCB_WINDOW_NONE,
-			XCB_NONE, XCB_BUTTON_INDEX_1, XCB_MOD_MASK_1/*ALT*/);
+//	/** grab alt-button1 move **/
+//	_base->_window->grab_button(true, DEFAULT_BUTTON_EVENT_MASK,
+//			XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_ASYNC, XCB_WINDOW_NONE,
+//			XCB_NONE, XCB_BUTTON_INDEX_1, XCB_MOD_MASK_1/*ALT*/);
+//
+//	/** grab alt-button3 resize **/
+//	_base->_window->grab_button(true, DEFAULT_BUTTON_EVENT_MASK,
+//			XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_ASYNC, XCB_WINDOW_NONE,
+//			XCB_NONE, XCB_BUTTON_INDEX_3, XCB_MOD_MASK_1/*ALT*/);
 
-	/** grab alt-button3 resize **/
 	_base->_window->grab_button(true, DEFAULT_BUTTON_EVENT_MASK,
 			XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_ASYNC, XCB_WINDOW_NONE,
-			XCB_NONE, XCB_BUTTON_INDEX_3, XCB_MOD_MASK_1/*ALT*/);
+			XCB_NONE, XCB_BUTTON_INDEX_1, XCB_MOD_MASK_ANY);
+
+	_base->_window->grab_button(true, DEFAULT_BUTTON_EVENT_MASK,
+			XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_ASYNC, XCB_WINDOW_NONE,
+			XCB_NONE, XCB_BUTTON_INDEX_2, XCB_MOD_MASK_ANY);
+
+	_base->_window->grab_button(true, DEFAULT_BUTTON_EVENT_MASK,
+			XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_ASYNC, XCB_WINDOW_NONE,
+			XCB_NONE, XCB_BUTTON_INDEX_3, XCB_MOD_MASK_ANY);
 
 }
 
